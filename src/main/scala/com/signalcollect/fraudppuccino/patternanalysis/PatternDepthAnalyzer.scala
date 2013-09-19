c262e5f8c48a5ea5b1c22cce750116acb567a44d
Daniel Strebel <daniel.strebel@gmail.com>
2013-09-19 13:37:23 +0200

    Pattern depth exploring

---
+++ src/main/scala/com/signalcollect/fraudppuccino/patternanalysis/PatternDepthAnalyzer.scala
@@ -0,0 +1,44 @@
+package com.signalcollect.fraudppuccino.patternanalysis
+
+import com.signalcollect.fraudppuccino.repeatedanalysis._
+import com.signalcollect._
+import com.signalcollect.fraudppuccino.structuredetection.DownstreamTransactionPatternEdge
+
+class PatternDepthAnalyzer(vertex: RepeatedAnalysisVertex[_]) extends VertexAlgorithm(vertex) with TransactionRelationshipExplorer {
+
+  var depth = 0
+
+  def getState = depth
+
+  def setState(state: Any) = {
+    state match {
+      case newDepth: Int => depth = newDepth
+      case _ =>
+    }
+  }
+
+  def deliverSignal(signal: Any, sourceId: Option[Any], graphEditor: GraphEditor[Any, Any]) = {
+    signal match {
+      case signaledDepth: Int => if (signaledDepth > depth) {
+        depth = signaledDepth
+        scoreSignal = 1.0
+      }
+      case _ =>
+    }
+    true
+  }
+
+  def executeSignalOperation(graphEditor: GraphEditor[Any, Any], outgoingEdges: Iterable[(Any, EdgeMarker)]) {
+    outgoingEdges.filter(_._2 == DownstreamTransactionPatternEdge).foreach(edge => graphEditor.sendSignal(depth+1, edge._1, Some(vertex.id)))
+  }
+
+  def executeCollectOperation(graphEditor: GraphEditor[Any, Any]) = {
+  }
+
+  var scoreSignal = if (isPatternSource) 1.0 else 0.0
+
+  var scoreCollect = 0.0
+
+  def noitfyTopologyChange {
+  }
+}
---
+++ src/test/scala/com/signalcollect/fraudppucchino/detection/PatternDepthSpec.scala
@@ -0,0 +1,72 @@
+package com.signalcollect.fraudppucchino.detection
+
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import org.specs2.mutable.SpecificationWithJUnit
+import com.signalcollect._
+import com.signalcollect.fraudppuccino.repeatedanalysis._
+import scala.collection.mutable.Map
+import com.signalcollect.fraudppuccino.structuredetection._
+import com.signalcollect.fraudppuccino.patternanalysis.PatternDepthAnalyzer
+
+@RunWith(classOf[JUnitRunner])
+class PatternDepthSpec extends SpecificationWithJUnit {
+  "Peeling Chains " should {
+    
+    sequential
+    
+    val graph = GraphBuilder.build
+
+    val transactionsMap = Map[Int, RepeatedAnalysisVertex[_]]()
+
+    val transactions = List((100, 500l, 0l, 0, 1),
+      (101, 450l, 1l, 1, 2),
+      (102, 50l, 1l, 1, 3),
+      (103, 400l, 2l, 2, 4),
+      (104, 50l, 2l, 2, 5),
+      (105, 350l, 3l, 4, 6),
+      (106, 50l, 3l, 4, 7))
+
+    for (transaction <- transactions) {
+      val tx = new RepeatedAnalysisVertex(transaction._1)
+      tx.storeAttribute("value", transaction._2)
+      tx.storeAttribute("time", transaction._3)
+      tx.storeAttribute("src", transaction._4)
+      tx.storeAttribute("target", transaction._5)
+      graph.addVertex(tx)
+      transactionsMap += ((tx.id, tx))
+    }
+
+    graph.addEdge(100, new DownstreamTransactionEdge(101))
+    graph.addEdge(100, new DownstreamTransactionEdge(102))
+    graph.addEdge(101, new UpstreamTransactionEdge(100))
+    graph.addEdge(102, new UpstreamTransactionEdge(100))
+
+    graph.addEdge(101, new DownstreamTransactionEdge(103))
+    graph.addEdge(101, new DownstreamTransactionEdge(104))
+    graph.addEdge(103, new UpstreamTransactionEdge(101))
+    graph.addEdge(104, new UpstreamTransactionEdge(101))
+
+    graph.addEdge(103, new DownstreamTransactionEdge(105))
+    graph.addEdge(103, new DownstreamTransactionEdge(106))
+    graph.addEdge(105, new UpstreamTransactionEdge(103))
+    graph.addEdge(106, new UpstreamTransactionEdge(103))
+
+    " be found" in {
+
+      graph.foreachVertex(v => v.asInstanceOf[RepeatedAnalysisVertex[_]].setAlgorithmImplementation(rav => new PatternDepthAnalyzer(rav)))
+
+      graph.recalculateScores
+      graph.execute
+
+      transactionsMap.get(100).get.state === 0
+      transactionsMap.get(101).get.state === 1
+      transactionsMap.get(102).get.state === 1
+      transactionsMap.get(103).get.state === 2
+      transactionsMap.get(104).get.state === 2
+      transactionsMap.get(105).get.state === 3
+      transactionsMap.get(106).get.state === 3
+    }
+  }
+
+}
