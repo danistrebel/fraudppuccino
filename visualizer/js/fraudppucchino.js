915fece4099f0e2b94c4f5fae3f0b2e3ef8ac440
Daniel Strebel <daniel.strebel@gmail.com>
2013-10-02 09:23:00 +0200

    added remove icons on the reports list

--- visualizer/js/fraudppucchino.js
+++ visualizer/js/fraudppucchino.js
@@ -1,11 +1,82 @@
+var width = 960,
+    height = 500;
+
+var color = d3.scale.category20();
+
+var force = d3.layout.force()
+    .charge(-500)
+    .linkDistance(5)
+    .size([width, height]);
+
+var svg = d3.select("svg#patternVisualizer");
+
+var reports = []
+	
+var graph = {
+  "nodes":[],
+  "links":[]
+}
+
 $(function() {
-	$("#tabs").tabs();
+	$("#tabs").tabs(); // Initialize Tabs
 });
 
 $('.navTab a').click(function(e) {
 	$(this).parent().siblings().removeClass('active');
 	$(this).parent().addClass('active');
 })
+
+function updateVisualization() {
+   
+	$('div #patternVisualizer').empty();
+  
+  force
+      .nodes(graph.nodes)
+      .links(graph.links)
+      .start();
+	  
+  svg.append("svg:defs").append("svg:marker")
+	      .attr("id", "transaction")
+	      .attr("viewBox", "0 -5 10 10")
+	      .attr("refX", 13)
+	      .attr("refY", 0)
+	      .attr("markerWidth", 6)
+	      .attr("markerHeight", 6)
+	      .attr("orient", "auto")
+	    .append("svg:path")
+	      .attr("d", "M0,-5L10,0L0,5");
+  
+
+  var link = svg.append("svg:g").selectAll(".link")
+      .data(graph.links)
+	  .enter().append("svg:path")
+	      .attr("class", "link")
+	      .attr("marker-end", "url(#transaction)")
+    
+
+  var node = svg.append("svg:g").selectAll(".node")
+      .data(graph.nodes)
+    .enter().append("circle")
+      .attr("class", "node")
+      .attr("r", 5)
+      .style("fill", function(d) { return color(d.group); })
+      .call(force.drag);
+
+  node.append("title")
+      .text(function(d) { return d.name; });
+
+  force.on("tick", function() {
+    link.attr("d", function(d) {
+    var dx = d.target.x - d.source.x,
+        dy = d.target.y - d.source.y;
+    return "M" + d.source.x + "," + d.source.y + "L" + d.target.x + "," + d.target.y;
+  });
+
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+  });
+  
+}
 
 function updateReports() {
 	$("div #reportsList").empty();
@@ -18,18 +89,19 @@ function appendReport(report) {
 	var index = reports.length;
 	reports.push(report);
 	var date = new Date(report.start);
-	var reportListEntry = '<a href="#" data-component-id="' + index
-			+ '" class="list-group-item">' + date.toLocaleDateString()
+	var reportListEntry = '<li data-component-id="' + index
+			+ '" class="list-group-item"><span class="glyphicon glyphicon-remove remove-report"></span>' + date.toLocaleDateString()
 			+ '<br/>' + report.members.length + ' Transactions<br/>$'
-			+ report.flow + '</a>';
+			+ report.flow + '</li>';
+	
 	$("div #reportsList").append(reportListEntry);
 	$("div #reportsHeader").text("Reports("+ reports.length +")");
 }
 
 $(document).on('click', '.list-group-item', function() {
-	$(this).siblings().removeClass('active');
-	$(this).addClass('active');
-	loadComponentMembers($(this).attr("data-component-id"));
+		$(this).siblings().removeClass('active');
+		$(this).addClass('active');
+		loadComponentMembers($(this).attr("data-component-id"));
 });
 
 function loadComponentMembers(id) {
@@ -37,7 +109,7 @@ function loadComponentMembers(id) {
 	graph.nodes = [];
 	graph.links = [];
 	
-	var transactionLookUp = {}; //Index on transactionId
+	var transactionLookUp = {}; // Index on transactionId
 
 	reports[id].members.forEach(function(transaction, index) {
 		var tx = {"name" : transaction.id, "group" : 1}
